32e362d6ca73f12aa6bd29201783a88189087ab4
Matthew Tanner <115193+matthew-tanner@users.noreply.github.com>
2021-06-15 23:48:27 -0500

    fixed Promise error with JWT validation

--- src/middleware/validate-jwt.js
+++ src/middleware/validate-jwt.js
@@ -4,31 +4,38 @@ const { UserModel } = require("../models");
 const validateJWT = async (req, res, next) => {
   if (req.method == "OPTIONS") {
     next();
-  } else if (req.headers.authorization && req.headers.authorization.includes("Bearer")) {
-    const { authorization } = req.headers;
-    const payload = authorization
-      ? jwt.verify(
-          authorization.includes("Bearer") ? authorization.split(" ")[1] : authorization,
-          process.env.JWT_SECRET
-        )
-      : undefined;
-
-    if (payload) {
-      let foundUser = await UserModel.findOne({
-        where: { id: payload.id },
-      });
-      if (foundUser) {
-        req.user = foundUser;
-        next();
+  }
+  
+  try{
+    if (req.headers.authorization && req.headers.authorization.includes("Bearer")) {
+      const { authorization } = req.headers;
+      const payload = authorization
+        ? jwt.verify(
+            authorization.includes("Bearer") ? authorization.split(" ")[1] : authorization,
+            process.env.JWT_SECRET
+          )
+        : undefined;
+  
+      if (payload) {
+        let foundUser = await UserModel.findOne({
+          where: { id: payload.id },
+        });
+        if (foundUser) {
+          req.user = foundUser;
+          next();
+        } else {
+          res.status(400).send({ message: "Not authorized" });
+        }
       } else {
-        res.status(400).send({ message: "Not authorized" });
+        res.status(401).send({ messaage: "Invalid token" });
       }
     } else {
-      res.status(401).send({ messaage: "Invalid token" });
+      res.status(403).send({ message: "forbidden" });
     }
-  } else {
-    res.status(403).send({ message: "forbidden" });
+  }catch(err){
+    res.status(500).send({message: err})
   }
+
 };
 
 module.exports = validateJWT;
